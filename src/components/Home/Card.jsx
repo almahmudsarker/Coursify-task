cb0a5a2c44b0eb83757dfda798e2cf5a527e92a3
almahmudsarker <almahmuds427@gmail.com>
2023-08-02 20:55:09 +0600

    card page added

---
+++ src/components/Home/Card.jsx
@@ -0,0 +1,48 @@
+// src/components/Card.js
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Card = () => {
+  const [jobOpenings, setJobOpenings] = useState([]);
+
+  useEffect(() => {
+    // Fetch job openings from the API
+    axios
+      .get("http://localhost:5000/api/jobOpenings")
+      .then((response) => {
+        setJobOpenings(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching job openings:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      {jobOpenings.map((job) => (
+        <div key={job.id} className="card w-96 bg-white shadow-xl mx-auto my-16">
+          <figure>
+            <img
+              src={job.image}
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {job.position}
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>{job.description}</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">{job.location}</div>
+              <div className="badge badge-outline">Remote</div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Card;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Card from './card';
 
 const Home = () => {
-    return <h1 className="text-6xl font-bold text-center">Hello world!</h1>;
+    return (
+      <>
+        <div>
+          <h1 className="text-3xl font-bold text-center py-10">
+            Welcome to our Recruitment Portal
+          </h1>
+          <p className="text-base font-bold text-center">
+            We connect companies with talented professionals.
+          </p>
+          <p className="text-sm font-bold text-center">
+            Join us to find your dream job or hire the best candidates.
+          </p>
+          <div className='flex flex-row'>
+            <Card />
+          </div>
+        </div>
+      </>
+    );
 };
 
 export default Home;
